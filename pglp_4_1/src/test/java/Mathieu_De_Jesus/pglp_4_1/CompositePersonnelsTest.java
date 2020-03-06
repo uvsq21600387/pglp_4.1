1bb4adf2340460aeaa0e6a660994f2b19d83bab1
Mathieu De Jesus <mathieu.de-jesus@ens.uvsq.fr>
2020-03-06 20:53:26 +0100

    tests unitaires

---
+++ pglp_4_1/src/test/java/Mathieu_De_Jesus/pglp_4_1/CompositePersonnelsTest.java
@@ -0,0 +1,44 @@
+package Mathieu_De_Jesus.pglp_4_1;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+
+/**
+ * tests unitaires sur la classe CompositePersonnels.
+ */
+public class CompositePersonnelsTest {
+	@Test
+	/**
+	 * teste le constructeur.
+	 */
+	public void testConstructeur() {
+		CompositePersonnels cp = new CompositePersonnels();
+		Iterator<InterfacePersonnels> ip = cp.iterator();
+		assertFalse(ip.hasNext());
+	}
+	@Test
+	/**
+	 * teste l'ajout de personnel.
+	 */
+	public void testAjout() {
+		CompositePersonnels cp = new CompositePersonnels();
+		cp.add(new CompositePersonnels());
+		Iterator<InterfacePersonnels> ip = cp.iterator();
+		assertTrue(ip.hasNext());
+	}
+	@Test
+	/**
+	 * teste la suppression de personnel.
+	 */
+	public void testSuppression() {
+		CompositePersonnels cp = new CompositePersonnels();
+		CompositePersonnels cp2 = new CompositePersonnels();
+		cp.add(cp2);
+		cp.remove(cp2);
+		Iterator<InterfacePersonnels> ip = cp.iterator();
+		assertFalse(ip.hasNext());
+	}
+}
---
+++ pglp_4_1/src/test/java/Mathieu_De_Jesus/pglp_4_1/PersonnelTest.java
@@ -0,0 +1,27 @@
+package Mathieu_De_Jesus.pglp_4_1;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+/**
+ * tests unitaires sur la classe Personnel.
+ * @author user
+ *
+ */
+public class PersonnelTest {
+
+	@Test
+	/**
+	 * teste le constructeur.
+	 */
+	public void test() {
+		ArrayList<String> numero = new ArrayList<String>();
+    	numero.add("06.18.12.15.95");
+    	numero.add("01.25.46.85.16");
+        Personnel p = new Personnel.Builder("man", "Iron", LocalDate.of(1955, 05, 02), numero).build();
+        p.print();
+	}
+
+}
